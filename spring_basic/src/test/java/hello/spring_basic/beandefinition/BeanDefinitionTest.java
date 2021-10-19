2a83ed57cde17b2128c87950eba4bd2053efeaff
joisfe <rhsnqk@gmail.com>
2021-10-19 15:24:35 +0900

    spring_basic(15)

---
+++ spring_basic/src/test/java/hello/spring_basic/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,29 @@
+package hello.spring_basic.beandefinition;
+
+import hello.spring_basic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext (AppConfig.class); // 어노테이션 기반 자바 코드로 된 AppConfig.java 설정 정보
+
+    // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml"); // xml 기반의 appConfig.xml 스프링 설정 정보
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
